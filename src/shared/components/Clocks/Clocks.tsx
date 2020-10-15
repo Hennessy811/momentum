fc00b4674de6e91bec429c8190df2cfef4be6602
Malakhov Dmitriy <dmitriy.malakhov@gorparkovki.ru>
2020-10-15 19:41:44 +0300

    greeting added

--- src/shared/components/Clocks/Clocks.tsx
+++ src/shared/components/Clocks/Clocks.tsx
@@ -1,19 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import { getTimeStr } from '../../utils/time';
+import { useLocalStorage } from 'react-use';
+import { getGreeting, getTimeStr } from '../../utils/time';
 import styles from './Clocks.module.scss';
 
 const Clocks = () => {
   const [timeString, setTimeString] = useState(getTimeStr());
+  const [greeting, setGreeting] = useState(getGreeting());
+  const [tempName, setTempName] = useState('');
+  const [name, setName] = useLocalStorage('username');
 
   useEffect(() => {
     setInterval(() => {
       setTimeString(getTimeStr());
+      setGreeting(getGreeting());
     }, 1000);
   }, []);
 
+  const input = (
+    <div>
+      <form
+        action=""
+        onSubmit={(e) => {
+          setName(tempName);
+        }}
+      >
+        <input
+          className={styles.inputName}
+          type="text"
+          value={tempName}
+          onChange={(e) => setTempName(e.target.value)}
+          placeholder="Enter your name"
+        />
+      </form>
+    </div>
+  );
+
   return (
-    <div className={styles.clock}>
-      {timeString}
+    <div className={styles.wrapper}>
+      <div className={styles.clock}>
+        {timeString}
+      </div>
+      {name ? (
+        <div className={styles.greeting}>
+          {greeting}
+          ,
+          {' '}
+          {name}
+        </div>
+      ) : input}
     </div>
   );
 };
--- src/shared/utils/time.ts
+++ src/shared/utils/time.ts
@@ -2,4 +2,21 @@ import { DateTime } from 'luxon';
 
 export const getTimeStr = () => DateTime.local().toLocaleString({ hour: 'numeric', minute: 'numeric', hour12: false });
 
+export const getGreeting = () => {
+  const data = [
+    [0, 4, 'Good night'],
+    [5, 11, 'Good morning'], // Store messages in an array
+    [12, 17, 'Good afternoon'],
+    [18, 23, 'Good evening'],
+    [24, 24, 'Good night'],
+  ];
+  const hr = new Date().getHours();
+
+  for (let i = 0; i < data.length; i++) {
+    if (hr >= data[i][0] && hr <= data[i][1]) {
+      return data[i][2];
+    }
+  }
+};
+
 export default getTimeStr;
